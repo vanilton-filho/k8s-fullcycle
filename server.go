63321772e0d2733aa4c7193853b35ede9e8de7f8
Vanilton Filho <vanilton.alv3sfilho@gmail.com>
2023-08-05 16:03:03 -0300

    feat: use targetPort

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
