74f32994cbf30e15e53d0b30c97e485e45e3e8eb
Vanilton Filho <vanilton.alv3sfilho@gmail.com>
2023-08-06 02:01:33 -0300

    feat(secrets): add secret to goserver

--- server.go
+++ server.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 
 	http.ListenAndServe(":8000", nil)
 }
@@ -49,4 +50,11 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "My family: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s\nPassword: %s", user, password)
 }
