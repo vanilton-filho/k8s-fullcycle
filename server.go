bbaf2cbc80a3088efd124f4cca3681555ed27bc4
Vanilton Filho <vanilton.alv3sfilho@gmail.com>
2023-08-09 19:39:23 -0300

    feat: add readinessprobe + livenessprobe

--- server.go
+++ server.go
@@ -67,7 +67,7 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 
 	duration := time.Since(startedAt)
 
-	if duration.Seconds() < 10 {
+	if duration.Seconds() < 10 || duration.Seconds() > 30 {
 		w.WriteHeader(500)
 		w.Write([]byte(fmt.Sprintf("Durantion: %v", duration)))
 	} else {
